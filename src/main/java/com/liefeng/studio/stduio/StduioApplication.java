25f4ea2dd2304285e462e5be91c4f3e7aa26b5de
figger <2695766757@qq.com>
2019-08-06 11:37:55 +0800

    figger 0.0.1 项目搭建

---
+++ src/main/java/com/liefeng/studio/stduio/StduioApplication.java
@@ -0,0 +1,13 @@
+package com.liefeng.studio.stduio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StduioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StduioApplication.class, args);
+    }
+
+}
